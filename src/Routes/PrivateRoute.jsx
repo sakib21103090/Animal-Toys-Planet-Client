e2472ab0bc93ede59e07aea8a421dfee16d71465
sakib21103090 <21103090@iubat.edu>
2023-05-18 13:00:54 +0600

    add private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 
 import { Navigate, useLocation } from 'react-router-dom';
-import { AuthContext } from '../Components/Providers/AuthProviders';
 
 
 
@@ -11,7 +10,7 @@ const PrivateRoute = ({children}) => {
 
     if(loading){
         return   <div>
-        <h3>loading..........</h3>
+        <h3>loading............</h3>
       </div>
     }
 
