f8ae570647adf1c0274dacc972b491faf6792e86
sakib21103090 <21103090@iubat.edu>
2023-05-18 11:17:56 +0600

    add signUp page

--- src/Components/LogPages/Login.jsx
+++ src/Components/LogPages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                <input type="text" name='email' placeholder=" Enter email" className="input input-bordered" />
                             </div>
                             <div className="form-control">
                                 <label className="label">
